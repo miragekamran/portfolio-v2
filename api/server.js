e486782a097ba85ea9cf612db8920a8b216a132b
Mirage Kamran <mirage.kamran.pkt@gmail.com>
2025-08-09 23:00:46 -0400

    email should be sent

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const sendEmail = require("./path/to/your/sendEmail");
-const app = express();
+// const express = require("express");
+// const sendEmail = require("./path/to/your/sendEmail");
+// const app = express();
 
-app.use(express.json());
+// app.use(express.json());
 
-app.post("/api/contact", sendEmail);
+// app.post("/api/contact", sendEmail);
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+// app.listen(3000, () => console.log("Server running on port 3000"));
